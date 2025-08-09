6c11bb5269eecca7d58c0b97f5d0ab3bc0e92422
Jakerulz <vickersjakob@gmail.com>
2025-08-08 22:07:05 -0600

    Initial commit

---
+++ src/main/java/net/jakerulz31/shardbound/Shardbound.java
@@ -0,0 +1,16 @@
+package net.jakerulz31.shardbound;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Shardbound implements ModInitializer {
+	public static final String MOD_ID = "shardbound";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/jakerulz31/shardbound/ShardboundClient.java
@@ -0,0 +1,10 @@
+package net.jakerulz31.shardbound;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ShardboundClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
